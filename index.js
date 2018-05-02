a3cdf7c4706ccc5fb968edae6cfe33ecad5e67ff
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-02 00:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function createPost(){
 
 function postComment(){
 
-  //var commenter = document.getElementById('commenterName').value
-  //var comment = document.getElementById('commentText').value
+  var commenter = document.getElementById('commenterName').value
+  var comment = document.getElementById('commentText').value
 
   var commentTemplate = document.getElementById('comment-template').innerHTML
 
